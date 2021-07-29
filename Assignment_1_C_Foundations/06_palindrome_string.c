f5e0f3c4a551508e158d175ccf4f57417ec94f53
Muskan <69207355+Muskan10Mehta@users.noreply.github.com>
2021-07-30 01:30:55 +0530

    Update and rename palindrome_string.c to 06_palindrome_string.c

--- Assignment_1_C_Foundations/06_palindrome_string.c
+++ Assignment_1_C_Foundations/06_palindrome_string.c
@@ -5,7 +5,7 @@
  * Execution: ./palindrome
  *
  * @Muskan, 190990681, 28/07/2021
- * Day 2 Assignment C Foundations, question 7
+ * Day 2 Assignment C Foundations, Question 7
  */
 
 #include <stdio.h>
@@ -26,10 +26,14 @@ int main(){
    //  simple %s stops taking input once space is encountered 
    scanf("%[^\n]s", input);
    
+   //to calculate string length
    for(i = 0; input[i]; i++){
        length++;
    }
    
+   // checks whether the first chracter matches with the last
+   // to ingore case of chracter we change the last digit to lowercase or to uppercase
+   // lowercase starts from a = 97, uppercase starts from A = 65
    if( input[0] == input[length-1]|| input[0] == input[length-1] + 32 || input[0] == input[length-1] - 32){
 	printf("true\n");
    } else{
