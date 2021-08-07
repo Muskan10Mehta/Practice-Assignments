4fc5a0a2145bb072154ae698da5f1887fe383bfd
vagrant <vagrant@myvm19>
2021-08-07 07:13:13 +0000

    updated ques 12

--- assignment_1_c_foundations/12_strindex.c
+++ assignment_1_c_foundations/12_strindex.c
@@ -1,83 +1,74 @@
 /**
- * Program that returns the rightmost index in string 1 where string 2 occurs
- * returns -1 if there is none
+ * Program to find the rightmost occurence of t in s, returns -1 if there is none
  *
- * Complilation: gcc -o any 11_any_match.c
- * Execution: ./any
+ * Compilation: gcc -o str_index 12_strindex.c
+ * Execution: ./str_index
  *
- * @Muskan, 1910990681, 29 July 2021
- * Assignment 1 C Foundations Question 12 
+ * @Muskan, 1910990681, 6/8/21
+ * Assignment 1 C Foundations, Question 12
  */
 
 #include <stdio.h>
 
-int strindex(char*, char*);
 int strlength(char*);
+int strindex(char*, char*);
 
 int main(){
-   
+
    char s1[50];
    char s2[50];
-   int index;
 
    printf("Enter first string: ");
    scanf("%s", s1);
 
    printf("Enter second string: ");
    scanf("%s", s2);
-   
-   index = strindex(s1, s2);
 
-   printf("%d\n", index);
+   printf("Rightmost occurence of String 2 in String 1:  %d\n", strindex(s1, s2));
 
    return 0;
 }
-int strlength(char s[]){
-    int i;
-    int len = 0;
 
-    for(i = 0; s[0]; i++){
+int strlength(char *s){
+
+   int len;
+   int i;
+
+   for(i = 0; s[i]; i++){
        len++;
-    }
-
-    return len;
+   }
+   
+   return len;
 }
 
-int strindex(char s1[], char s2[]){
-   
-   int index = 0;
-   int flag = 0;
-   int i, j;
-   
-   int s1_len = strlength(s1);
-   int s2_len = strlength(s2);
+int strindex(char *s1, char *s2){
 
-   i = s1_len - 1;
-   j = s2_len - 1;
-   
+  int i, j, k;
+  int flag = 0;
+  int index = -1;
 
-   // matching from the end
-   // from the end the first index we find will be the rightmost index
-
-   while(i >= 0){
-        j = s2_len - 1;
-	while(s2[j] == s1[i]){
-		i--;
-		j--;
+  for(i = 0; s1[i]; i++){
+  
+     if(s1[i] == s2[0]){
+        
+	k = i + 1;
+        for(j = 1; s2[j]; j++){
+	   
+	   if(s1[k] == s2[j]){
+	         flag = 1;
+	   } else{
+	         flag = 0;
+		 break;
+ 	   }
+         
 	}
+	
+       if(flag == 1){
+           index = i;
+       }
+     }
+  
+  }
 
-	if(j == 0){
-	   flag = 1;
-	   index = i;
-	   break;
-	}
-
-	i--;
-   }
-
-
-   if(flag == 1){
-      return index;
-   }else {
-       return -1;}
+  return index;
 }
