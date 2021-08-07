f8a422599b9303a57f018a38fc780edb52aebc73
Muskan10Mehta <vagrant@myvm19>
2021-08-07 18:29:15 +0000

    updated question 7

--- assignment_1_c_foundations/07_print_lines.c
+++ assignment_1_c_foundations/07_print_lines.c
@@ -4,29 +4,59 @@
  * Complilation: gcc -o file 07_print_lines.c
  * Execution: ./file
  *
- * @Muskan, 1910990681, 29 July 2021
+ * @Muskan, 1910990681, 31 July 2021
  * Assignment 1 C Foundations Question 7
- *
+ * Resources: w3resource.com (to revise file system in c), geeksforgeeks to read about fgets and fscanf
  */
 
 #include <stdio.h>
 
 int main(){
    
-   FILE *f = fopen("07_text.txt", "r");
+   int i = 0;
+   char store_line[500];
+   int length = 0;
+   char c;
 
+   // creates a pointer to the file 
+   // opens the file  
+   // r is for read-only
+
+   FILE *f = fopen( "07_text.txt", "r" );
+
+   // checks if file is empty or not
    if(f == NULL){
    
        printf("File not found");
 
    } else{
-
-       char file[100], 
-
+         
+       // fgcanf reads from the given file
+       // it takes a pointer to file, 
+       // character input format, we can get the characters we want from the file
+       // array or buffer to store the scanned characters in
+       
+       // here we are scanning lines until \n is encountered and storing them in store_line array until we reach enf of file	   
+       while(fscanf(f, "%[^\n]%*c", store_line) != EOF){
+           
+	   length = 0;
+           for(i = 0; store_line[i]; i++){
+	       
+	       length++;
+	   
+	   }
+           
+	   if(length > 80){
+	   
+		printf("%s", store_line);
+	    
+	   }
+       
+       }
 
    }
 
    fclose(f);
    
    return 0;
-}
+}	
