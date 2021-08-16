e6b0dc9819185d51024b6ef55fb5af9d6f31f0dd
Muskan10Mehta <vagrant@myvm19>
2021-08-16 16:16:00 +0000

    added new empty files

---
+++ assignment_4_stacks_and_queues/05_min_stack.c
@@ -0,0 +1,15 @@
+/** 
+ * Program to 
+ *
+ * Compilation: gcc -o 
+ * Execution: ./
+ *
+ * @Muskan, 1910990681, 09/08/2021
+ * Assignment 4 Stacks and Queues, Question 
+ */
+
+#include <stdio.h>
+
+int main(){
+   return 0;
+}
---
+++ assignment_4_stacks_and_queues/06_sliding_window.c
@@ -0,0 +1,24 @@
+/**
+ * Program for this ques: You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right.
+ * You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+ * Return the max sliding window.
+ *
+ * Compilation: gcc -o window 06_sliding_window.c
+ * Execution: ./window
+ *
+ * @Muskan, 1910990681, 15/08/2021
+ * Assignment 4 Stacks and Queues, Question 6
+ * Resources: Geeksforgeeks(for struct)
+ */
+
+#include <stdio.h>
+
+int main(){
+
+   int i;
+   int size;
+
+
+
+   return 0;
+}
