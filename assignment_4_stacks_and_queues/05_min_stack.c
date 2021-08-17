807b2c4b244cc31748e42a98bc6138942af1c711
Muskan10Mehta <vagrant@myvm19>
2021-08-17 09:41:55 +0000

    updated min stack

--- assignment_4_stacks_and_queues/05_min_stack.c
+++ assignment_4_stacks_and_queues/05_min_stack.c
@@ -1,15 +1,87 @@
 /** 
- * Program to 
+ * Program to find minimum element from stack
  *
- * Compilation: gcc -o 
- * Execution: ./
+ * Coded on Leetcode: https://leetcode.com/submissions/detail/539821668/
  *
  * @Muskan, 1910990681, 09/08/2021
  * Assignment 4 Stacks and Queues, Question 
  */
 
-#include <stdio.h>
+typedef struct {
 
-int main(){
-   return 0;
+    int top;
+    int size;
+    int *array;
+
+} MinStack;
+
+/** initialize your data structure here. */
+
+MinStack* minStackCreate() {
+
+    MinStack* minStack = (MinStack*)malloc(sizeof(MinStack));
+    minStack->size = 30000;
+    minStack->top = -1;
+    minStack->array = (int*)malloc(minStack->size * sizeof(int));
+
+    return minStack;
 }
+
+void minStackPush(MinStack* obj, int val) {
+
+    if(!(obj->top >= obj->size)){
+
+        obj->top++;
+        obj->array[obj->top] = val;
+
+    }
+}
+
+void minStackPop(MinStack* obj) {
+   int val;
+
+   if(!(obj->top == -1)){
+
+	val = obj->array[obj->top];
+    obj->top--;
+   }
+
+   return val;
+}
+
+int minStackTop(MinStack* obj) {
+  return obj->array[obj->top];
+}
+
+int minStackGetMin(MinStack* obj) {
+
+    int i = 0;
+    int min;
+    min = obj->array[0];
+
+    for(i = 0; i <= obj->top; i++){
+
+         if(obj->array[i] < min){
+             min = obj->array[i];
+         }
+    }
+    return min;
+}
+
+void minStackFree(MinStack* obj) {
+    obj->top = -1;
+}
+
+/**
+ * Your MinStack struct will be instantiated and called as such:
+ * MinStack* obj = minStackCreate();
+ * minStackPush(obj, val);
+
+ * minStackPop(obj);
+
+ * int param_3 = minStackTop(obj);
+
+ * int param_4 = minStackGetMin(obj);
+
+ * minStackFree(obj);
+*/
