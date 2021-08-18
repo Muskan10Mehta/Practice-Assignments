f7b0db0f1ad0df2f510657edad38008dc2c99fe1
Muskan10Mehta <vagrant@myvm19>
2021-08-18 11:56:27 +0000

    added file

---
+++ assignment_4_stacks_and_queues/02_infix_to_postfix.c
@@ -0,0 +1,16 @@
+/** Program to convert infix to postfix
+ *
+ * Compilation: gcc -o postfix 02_infix_to_postfix.c
+ * Execution: ./postfix
+ *
+ * @Muskan, 1910990681, 10/08/2021
+ * Assignment 4 Stacks and Queues, Question 2
+ */
+
+#include <stdio.h>
+
+int main(){
+
+
+   return 0;
+}
