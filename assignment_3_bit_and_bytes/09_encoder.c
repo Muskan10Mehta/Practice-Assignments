23017d20ff885f7e1778550389302e1f600c50e6
Muskan10Mehta <vagrant@myvm19>
2021-08-18 12:15:23 +0000

    added file

---
+++ assignment_3_bit_and_bytes/09_encoder.c
@@ -0,0 +1,21 @@
+/** 
+ * Program to convert 32-bit to 64-bit to make an encoder and decoder
+ *
+ * Compilatiion: gcc -o encoder 09_encoder.c
+ * Execution: ./encoder
+ *
+ * @Muskan, 1910990681, 16/08/2021
+ * Assignment 3 Bits and Bytes, Question 9
+ * Resources:
+ */
+
+#include <stdio.h>
+
+int main(){
+
+
+
+
+   return 0;
+
+}
