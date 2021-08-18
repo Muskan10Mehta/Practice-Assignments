266bce2ea36c6a6b7cfba8abf04139c71d00066b
Muskan10Mehta <vagrant@myvm19>
2021-08-18 18:26:27 +0000

    updated file

--- assignment_4_stacks_and_queues/02_infix_to_postfix.c
+++ assignment_4_stacks_and_queues/02_infix_to_postfix.c
@@ -9,8 +9,115 @@
 
 #include <stdio.h>
 
+#define SIZE 100
+
+char stack[SIZE];
+int top = -1;
+
+// Checks if stack if full or not
+int isFull(){
+
+   if(top + 1 >= SIZE){
+        return 1;
+   } else {
+        return 0;
+   }
+
+}
+
+// Checks if stack is empty or not
+int isEmpty(){
+
+   if(top == -1){
+        return 1;
+   } else {
+        return 0;
+   }
+}
+
+// Adds a new element to the stack
+void push(char value){
+
+    if(!(isFull())){
+          stack[++top] = value;
+    }
+}
+
+// Remove and return the element from the stack.
+char pop(){
+ char element = 0;
+
+    if(!(isEmpty())){
+
+       element = stack[top];
+       stack[top] = -1;
+       top--;
+
+    }
+
+    return element;
+}
+
+// Returns the element at the top of stack
+int peek(){
+
+   return stack[top];
+
+}
+
+int precedence_order(char op){
+
+  if(op == '*' || op == '/' || op == '%'){
+       return 3;
+  }else if(op == '+' || op == '-'){
+       return 2;
+  }else{
+       return 1;
+  }
+
+  return 0;
+}
+
+void infix_to_postfix(char *infix){
+
+   int i = 0;
+   
+   char in;
+
+   for(i = 0; infix[i]; i++){
+     
+       if(infix[i] >= 48 && infix[i] <= 57){
+           printf("%c", infix[i]);
+       } else if(infix[i] == '('){
+           push(infix[i]);
+       } else if(infix[i] == ')'){
+           while(peek() != '(' && !(isEmpty())){
+               printf("%c\n", pop());
+           }
+       } else {
+           
+           while(precedence_order(infix[i]) <= precedence_order(peek()) && !(isEmpty())){
+                printf("%c", pop());
+           }
+           
+          push(infix[i]);
+       }
+   }
+   
+   while(!(isEmpty())){
+       printf("%c\n ", pop());
+   }
+}
+
 int main(){
 
+   char infix[100];
+   printf("Enter the infix expression: ");
+   scanf("%[^\n]s", infix);
+
+   infix_to_postfix(infix);
 
    return 0;
+
 }
+
